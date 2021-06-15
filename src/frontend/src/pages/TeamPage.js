59f671020b9b0e81d0acf08e7c75ff33997b3784
Joel Sunny <joelsunnygeorge@gmail.com>
2021-06-15 17:19:25 +0530

    Created Team Page Component

---
+++ src/frontend/src/pages/TeamPage.js
@@ -0,0 +1,11 @@
+import { React } from 'react';
+
+export const TeamPage = () => {
+  return (
+    <div className="TeamPage">
+      <h1>Team Name</h1>
+      <h3>Latest Matches</h3>
+    </div>
+  );
+}
+
