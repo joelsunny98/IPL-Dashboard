a3c565f55b4c85ac2b51cffdc29c630b564773a9
Joel Sunny <joelsunnygeorge@gmail.com>
2021-06-16 13:34:29 +0530

    Added Controller and Repository Packages

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,10 +1,30 @@
-import { React } from 'react';
+import { React, useEffect, useState } from 'react';
+import { MatchDetailCard } from '../components/MatchDetailCard'; 
+import { MatchSmallCard } from '../components/MatchSmallCard'; 
+
 
 export const TeamPage = () => {
+
+    const [team, setTeam] = useState();
+
+    useEffect(
+        () => {
+            const fetchMatches = async () => {
+                const response = await fetch('');
+                const data = await response.json();
+                setTeam(data);
+            };
+            fetchMatches();
+        }
+    )
+
   return (
     <div className="TeamPage">
       <h1>Team Name</h1>
-      <h3>Latest Matches</h3>
+      <MatchDetailCard />
+      <MatchSmallCard />
+      <MatchSmallCard />
+      <MatchSmallCard />
     </div>
   );
 }
--- src/main/java/io/javabrains/ipldashboard/model/Team.java
+++ src/main/java/io/javabrains/ipldashboard/model/Team.java
@@ -1,9 +1,12 @@
 package io.javabrains.ipldashboard.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 public class Team {
@@ -15,6 +18,8 @@ public class Team {
     private long totalMatches;
     private long totalWins;
 
+    @Transient 
+    private List<Match> matches;
 
     public long getId() {
         return id;
@@ -47,6 +52,17 @@ public class Team {
     @Override
     public String toString() {
         return "Team [teamName=" + teamName + ", totalMatches=" + totalMatches + ", totalWins=" + totalWins + "]";
+    }
+
+    public Team(){
+        
+    }
+
+    public List<Match> getMatches() {
+        return matches;
+    }
+    public void setMatches(List<Match> matches) {
+        this.matches = matches;
     }
 
     
