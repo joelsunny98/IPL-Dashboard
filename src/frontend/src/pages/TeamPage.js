bf7d2ef5b8785ae330e27d3805b0749528397e34
Joel Sunny <joelsunnygeorge@gmail.com>
2021-06-18 09:02:34 +0530

    React App with Team Page

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -5,26 +5,24 @@ import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const TeamPage = () => {
 
-    const [team, setTeam] = useState();
+    const[team, setTeam] = useState({matches:[]});
 
     useEffect(
         () => {
             const fetchMatches = async () => {
-                const response = await fetch('');
+                const response = await fetch('http://localhost:8080/team/Delhi%20Capitals');
                 const data = await response.json();
                 setTeam(data);
             };
             fetchMatches();
-        }
-    )
+        }, [] 
+    ); 
 
   return (
     <div className="TeamPage">
-      <h1>Team Name</h1>
-      <MatchDetailCard />
-      <MatchSmallCard />
-      <MatchSmallCard />
-      <MatchSmallCard />
+      <h1>{team.teamName}</h1>
+      <MatchDetailCard match ={team.matches[0]}/>
+      {team.matches.slice(1).map(match => <MatchSmallCard match={match}/>)}
     </div>
   );
 }
